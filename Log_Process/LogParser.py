c0a8d01a6087431d04ccb3ff73b4a58857335da4
Executor <executor.tw@gmail.com>
2012-05-09 12:37:01 +0800

    Removing country check in LogParser.py 	modified:   Log_Process/LogParser.py 	modified:   Log_Process/LogProcessor.py

--- Log_Process/LogParser.py
+++ Log_Process/LogParser.py
@@ -10,7 +10,9 @@ import re
 normalizer = LN('/usr/share/logsparser/normalizers')
 locator = GeoIP.new(GeoIP.GEOIP_MEMORY_CACHE)
 
-def Parser(filename):
+
+
+def Parser(filename,source):#source specify the localhost we are processing
     auth_logs = open(filename,'r')
     failed_logins = []
     failed_login = []
@@ -23,41 +25,33 @@ def Parser(filename):
             print "RAW:", log 
 	try:
             if l.get('action') == 'fail':
-	        country = locator.country_name_by_addr(l['source_ip'])
-	        if country:
-		    #IF GeoIP could identify where the login comes from
-                    failed_login = [l.get('date'),l.get('source'),l.get('source_ip'),country,l.get('user')]
-		    if DEBUG >= 3:
-			print "Failed with country located:" , failed_login
-	            failed_logins.append(failed_login)
-                else:
-	            #If GeoIP couldn't identify where the login comes from
-	            failed_login = [l.get('date'),l.get('source'),l.get('source_ip'),"Unknown",l.get('user')]
-		    if DEBUG >= 3:
-			print "Failed with no country:", failed_login
-		    failed_logins.append(failed_login)
+		failed_login = [l.get('date'),l.get('source_ip'),l.get('user')]
+		if DEBUG >= 3:
+		    print "Failed:" , failed_login
+		failed_logins.append(failed_login)
             elif l.get('action') == 'accept':
-	        country = locator.country_name_by_addr(l['source_ip'])
-	        if country:
-                #IF GeoIP could identify where the login comes from
-	            accept_login = [l.get('date'),l.get('source'),l.get('source_ip'),country,l.get('user')]
-		    if DEBUG >= 3:
-			print "Successful login with country located:", accept_login
-		    accept_logins.append(accept_login)
-                else:
-	            #If GeoIP couldn't identify where the login comes from
-	            accept_login = [l.get('date'),l.get('source'),l.get('source_ip'),"Unknown",l.get('user')]
-		    if DEBUG >= 3:
-			print "Successful login without country located:", accept_login
-		    accept_logins.append(accept_login)
+	        accept_login = [l.get('date'),l.get('source_ip'),l.get('user')]
+		if DEBUG >= 3:
+		    print "Successful login: ", accept_login
+		accept_logins.append(accept_login)
 	
 	except:
 	    print "Exception!"
 	    print "Excepted RAW:",log
 	    print "Excepted process:", accept_login
 
-    print "FAILED:",failed_logins
-    print "++++++++++++++++++++++++++++++++++++++++++++++====================================+++++++++++++++++++++++++++++++++"
+	    
+    if DEBUG >= 2 :
+        print "FAILED:"
+    	failed_logins.sort()
+	for fail in failed_logins:
+	    print fail
+        print "++++++++++++++++++++++++++++++++++++++++++++++====================================+++++++++++++++++++++++++++++++++"
+        print "ACCEPTED:" 
+	accept_logins.sort()
+        for accept in accept_logins:
+	   print accept
 
-    print "ACCEPTED:", accept_logins
-    print type(failed_logins[0][0])
+    return (failed_logins, accept_logins)
+
+
--- Log_Process/LogProcessor.py
+++ Log_Process/LogProcessor.py
@@ -2,12 +2,27 @@
 This file is to read the syslog report and find the BFA, Login message.
 """
 
-
+DEBUG = 2
 import sys
 import LogParser
 
-if len(sys.argv) != 2:
-    print "Usage:\n\t>python LogProcessor.py <path>.\nFor example:\n\t>python LogProcessor.py /var/log/241/1/1-auth.log"
+if len(sys.argv) != 3:
+    print "Usage:\n\t>python LogProcessor.py <file> <source>.\nFor example:\n\t>python LogProcessor.py /var/log/241/1/1-auth.log 140.117.205.1"
 else:
     filename = sys.argv[1]
-    LogParser.Parser(filename)
+    source = sys.argv[2]
+    failed_logins, accept_logins = LogParser.Parser(filename,source)
+
+    if DEBUG >= 2 :
+        print "In Main:"
+        print "FAILED:"
+        failed_logins.sort()
+        for fail in failed_logins:
+            print fail
+        print "++++++++++++++++++++++++++++++++++++++++++++++====================================+++++++++++++++++++++++++++++++++"
+        print "ACCEPTED:"
+        accept_logins.sort()
+        for accept in accept_logins:
+           print accept
+
+
